163170f4f9d231c83867954ca5d2e4ddc810babc
Bryce Wilbur <wilburbryce321@gmail.com>
2023-01-22 22:34:29 -0600

    made changes to britain and germany file

--- nation_state/britain.py
+++ nation_state/britain.py
@@ -1,3 +1,34 @@
+leaders = {
+    """Dictionary for leaders
+    Leader selection will be in sync with time frame selection
+    Unlike population, leader dictionary will be setup to be as historically accurate as possible"""
+    
+    "1910" : "George V",
+    "1914" : "George V",
+    "1918" : "George V",
+    "1932" : "Ramsey McDonald",
+    "1936" : "Stanley Baldwin",
+    "1939" : "Winston Churchill"
+}
+
+population = {
+    """Dictionary for population
+    Population selection will be in sync with time frame selection
+    Population will then be set up to grow or shrink in random amounts"""
+    
+    "1910" : 44915900,
+    "1914" : 42956900,
+    "1918" : 39582000,
+    "1932" : 46335000,
+    "1936" : 47081300,
+    "1940" : 46029200
+}
+class Britain:
+    def __init__(self):
+        self.leader = ""
+        self.population = 0
+        # leader isn't initialized until time_frame is established.
+
 def main():
     print("hi")
 
---
+++ nation_state/germany.py
@@ -0,0 +1,28 @@
+leaders = {
+    "1910" : "Wilhelm II",
+    "1914" : "Wilhelm II",
+    "1918" : "Friedrich Ebert",
+    "1932" : "Paul Von Hindenburg",
+    "1936" : "Adolf Hitler",
+    "1939" : "Adolf Hitler"
+}
+
+population = {
+    "1910" : 63200000,
+    "1914" : 63200000,
+    "1918" : 62400000,
+    "1932" : 67200000,
+    "1936" : 69100000,
+    "1939" : 70500000
+}
+class Germany:
+    def __init__(self):
+        self.leader = ""
+        self.population = 0
+        # leader isn't initialized until time_frame is established.
+
+def main():
+    print("hi")
+
+if __name__ == '__main__':
+    main()
