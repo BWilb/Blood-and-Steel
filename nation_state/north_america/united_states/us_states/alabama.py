16b6a71b69ba827ac438fc33e04880a723abbb8c
Bryce Wilbur <wilburbryce321@gmail.com>
2023-07-07 06:20:30 -0500

    edited Alabama

--- nation_state/north_america/united_states/us_states/alabama.py
+++ nation_state/north_america/united_states/us_states/alabama.py
@@ -55,6 +55,7 @@ class Alabama:
         self.economic_stimulus = False
         # military
         # other
+        self.master_nation = us
     # population functions
     def population_change(self):
         """instead of having the headache of calling both national objects separately, why not combine them"""
@@ -119,47 +120,25 @@ class Alabama:
     # economic functions
     def check_economic_state(self):
         """function dealing with primary economic decisions of canadian parliament"""
-        if self.date > self.economic_change_date:
-            """instead of comparing an entire year, break the year up into sections"""
-            if self.current_gdp > self.past_gdp:
-                if self.e_s.lower() == "recovery":
-                    self.e_s = "expansion"
-                    print("Your economy is now in an expansionary period.\n")
-                    time.sleep(3)
+        if self.master_nation.e_s == "recession":
+            self.recession()
 
-                elif self.e_s.lower() == "recession" or self.e_s.lower() == "depression":
-                    self.e_s = "recovery"
-                    print("Your economy is now in recovery period.\n")
-                    time.sleep(3)
+        elif self.master_nation.e_s == "recovery":
+            self.recovery()
 
-            elif self.current_gdp < self.past_gdp:
-                if self.e_s.lower() == "recession":
-                    self.e_s = "depression"
-                    print("Your economy is now in a recessionary period.\n")
-                    time.sleep(3)
+        elif self.master_nation.e_s == "depression":
+            self.depression()
 
-                elif self.e_s.lower() == "recovery" or self.e_s.lower() == "expansion":
-                    self.e_s = "recession"
-                    print("Your economy is now in a depression period.\n")
-                    time.sleep(3)
-        else:
-            if self.e_s == "recession":
-                self.recession()
-
-            elif self.e_s == "recovery":
-                self.recovery()
-
-            elif self.e_s == "depression":
-                self.depression()
-
-            elif self.e_s == "expansion":
-                self.expansion()
+        elif self.master_nation.e_s == "expansion":
+            self.expansion()
     def recession(self):
         if self.economic_stimulus:
 
             self.consumer_spending = -round(random.uniform(10, 150), 2)
             self.government_spending = round(random.uniform(100, 600), 2)
             self.national_debt += round(
+                (-self.consumer_spending + self.government_spending) * round(random.uniform(0.15, 0.35), 4), 2)
+            self.master_nation.national_debt += round(
                 (-self.consumer_spending + self.government_spending) * round(random.uniform(0.15, 0.35), 4), 2)
             self.investment = round(random.uniform(50, 350), 2)
             self.exports = round(random.uniform(10, 45), 2)
@@ -168,16 +147,23 @@ class Alabama:
             self.current_gdp += (self.consumer_spending + self.investment + self.government_spending +
                                  (self.exports - self.imports))
 
+            self.master_nation.current_gdp += (self.consumer_spending + self.investment + self.government_spending +
+                                 (self.exports - self.imports))
+
         else:
             self.consumer_spending = -round(random.uniform(10, 200), 2)
             self.government_spending = round(random.uniform(100, 700), 2)
             self.national_debt += round((-self.consumer_spending + self.government_spending) * round(random.uniform(0.15, 0.35), 4), 2)
+            self.master_nation.national_debt += round(
+                (-self.consumer_spending + self.government_spending) * round(random.uniform(0.15, 0.35), 4), 2)
             self.investment = -round(random.uniform(100, 500), 2)
             self.exports = round(random.uniform(10, 30), 2)
             self.imports = round(random.uniform(10, 105), 2)
 
             self.current_gdp += (self.consumer_spending + self.investment + self.government_spending +
                                     (self.exports - self.imports))
+            self.master_nation.current_gdp += (self.consumer_spending + self.investment + self.government_spending +
+                                               (self.exports - self.imports))
     def recovery(self):
         if self.economic_stimulus:
             self.consumer_spending = round(random.uniform(10, 450), 2)
