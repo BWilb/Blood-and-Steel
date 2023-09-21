36beed2fcee76d7b15618c54890c6455c6747a73
Bryce Wilbur <wilburbryce321@gmail.com>
2023-09-20 21:18:58 -0500

    made advancements with multipolygons within multi_test file

--- testing/multi_test.py
+++ testing/multi_test.py
@@ -1,22 +1,102 @@
-import json
-from shapely.geometry import MultiPolygon
+"""import geopandas as gpd
 
-"""def extract_coordinates(json_data):
-    coordinates = []
+# Load your GeoJSON data into a GeoDataFrame
+gdf = gpd.read_file('../nation_data/custom.geo (3).json')  # Replace with the path to your GeoJSON file
 
-    # Load JSON data
-    data = json.loads(json_data)
+# Use explode to convert MultiPolygons into individual polygons
+individual_polygons = gdf.explode()
+for i in range(0, len(gdf)):
+    if "MultiPolygon" in gdf['geometry']:
+        print(gdf['geometry'][i])
+    else:
+        print('none')
 
-    # Convert to MultiPolygon using Shapely
-    multi_polygon = MultiPolygon(data['coordinates'])
+# Print the resulting GeoDataFrame with individual polygons
+print(individual_polygons)"""
+import time
 
-    if multi_polygon.geom_type == 'MultiPolygon':
-        for polygon in multi_polygon:
-            if polygon.geom_type == 'Polygon':
-                for point in polygon.exterior.coords:
-                    coordinates.append(point)
+"""import geopandas as gpd
+import shapely.geometry as sg
 
-    return coordinates
+# Load your GeoJSON data into a GeoDataFrame
+gdf = gpd.read_file('../nation_data/custom.geo (3).json')  # Replace with the path to your GeoJSON file"""
+"""print(gdf.head())
+print(gdf['geometry'][4])"""
+"""print(len(gdf))
+individual = gdf.explode()
+print(len(individual))
+for id in range(0, len(individual['name']) - 1):"""
+# print(individual['name'][id], individual['geometry'][id])
+""" if individual['name'][id] == "Kazakhstan":
+    for coords in range(0, len(individual['geometry'][id])):
+        print()"""
+# print(individual['name'][id].any())
+# print(individual['name'][id], individual['geometry'][id])
+"""print(len(individual['geometry'][id]))
+time.sleep(0.5)"""
+# for j in range(0, id):
+# print(individual['id'])
+# print(individual)
+i = 0
+# name = individual['id'][0]
+# print(name)
+"""for geom in individual['name']:
+    if geom == "American Samoa":
+        print(individual['geometry'][i])
+    i += 1"""
+# Extract individual polygons from MultiPolygons
+individual_polygons = []
+"""for geom in gdf['geometry']:
+    print(geom.geom_type)"""
 
-extracted_coordinates = extract_coordinates(json_data)
-print(extracted_coordinates)"""
+# if geom.geom_type == 'MultiPolygon':
+"""for i in range(0, len(individual['name'])):
+    print(individual[i])"""
+
+"""individual_polygons.extend(geom)
+else:
+individual_polygons.append(geom)"""
+
+# Create a list of Shapely Polygon objects
+# shapely_polygons = [sg.Polygon(p.exterior.coords) for p in individual_polygons]
+
+# Now, you have a list of Shapely Polygon objects
+"""for polygon in shapely_polygons:
+    print(polygon)"""
+
+import geopandas as gpd
+
+# Load the GeoJSON data
+gdf = gpd.read_file('../nation_data/custom.geo (3).json').explode()
+
+
+# Define the nation name you want to search for
+target_nation = 'Netherlands'  # Replace with the nation you're interested in
+
+# Filter the GeoDataFrame to get the specific nation
+nation_data = gdf[gdf['name'] == target_nation]
+
+# Extract the coordinates as a list
+coordinates = []
+# coordinates represents the entire list of polygons within nation
+
+for geometry in nation_data.geometry:
+    if geometry.geom_type == 'Polygon':
+        coordinates.append(list(geometry.exterior.coords))
+    elif geometry.geom_type == 'MultiPolygon':
+        for polygon in geometry:
+            coordinates.append(list(polygon.exterior.coords))
+
+# Print the extracted coordinates
+for outer_coords in range(0, len(coordinates)):
+    #print(coordinates[outer_coords])
+    # outer coords represent the entire list of coordinates within each polygon
+    for inner_coords in range(0, len(coordinates[outer_coords])):
+        print(coordinates[outer_coords][inner_coords][0], coordinates[outer_coords][inner_coords][1])
+        #for index, row in range(0, len(coordinates[outer_coords][inner_coords])):
+            #print()
+
+"""
+extracting coordinates from geojson based upon nation name, using json, geopandas, and python
+ChatGPT
+"""
